a1e590dc129c873a081e0442e55ab5f725a57b54
dayuan <2191mma@gmail.com>
2022-04-01 23:49:07 +0800

    Update home.dart

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:night_read/common/global.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:night_read/utils/color.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({
@@ -15,28 +13,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Hi'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              AppLocalizations.of(context)!.helloWorld,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Reserve Player'),
+          backgroundColor: HexColor('#1e2632'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await Global.logout();
-
-          Navigator.pushReplacementNamed(context, 'login');
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Row(
+          children: [
+            Expanded(
+                child: Column(
+              children: [Text('hello world')],
+            ))
+          ],
+        ));
   }
 }
